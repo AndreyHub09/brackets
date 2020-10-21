064b9d27d58dcd2c4cd32356f7de2b447d6a8c78
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2020-10-21 12:34:20 +0300

    feat: brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (str.length > 0) {
+    const tempStr = checkRecurs(str, bracketsConfig, 0);
+    if (tempStr === str && tempStr !== '') {
+      return false;
+    }
+    str = tempStr;
+  }
+  return true;
 }
+
+function checkRecurs(str, bracketsConfig, index) {
+  const confElement = bracketsConfig.find(x => {
+    return x[0] === str[index] || x[1] === str[index];
+  });
+
+  if (confElement === undefined || str[index] !== confElement[0] || (index + 1) >= str.length) {
+    return str;
+  }
+
+  if (str[index + 1] === confElement[1]) {
+    return str.substring(0, index) + str.substring(index + 2, str.length);
+  }
+
+  return checkRecurs(str, bracketsConfig, index + 1);
+};
